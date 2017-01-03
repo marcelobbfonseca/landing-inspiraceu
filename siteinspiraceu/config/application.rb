720b14eadbc3ed9d446f17376caafcced99be356
marcelo <marcelobbff@gmail.com>
2017-01-03 13:08:57 -0200

    importado o thema do bootstrap da maneira bonitinha e politicamente correta

--- siteinspiraceu/config/application.rb
+++ siteinspiraceu/config/application.rb
@@ -8,8 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Siteinspiraceu
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Enable the asset pipeline
+    config.assets.enabled = true  
+    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
   end
 end
