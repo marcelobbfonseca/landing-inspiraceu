f1e3fcbab3e303d17486642348005595f0e4c5d7
marcelo <marcelobbff@gmail.com>
2017-01-04 20:09:53 -0200

    Refeito em cima do template Agora vai

--- siteinspiraceu/config/routes.rb
+++ siteinspiraceu/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'welcome#index'
-  root to: 'high_voltage/pages#show' , id: 'landing'
+  #root to: 'high_voltage/pages#show' , id: 'landing'
+  root 'creatives#index'
+  get 'creatives/index'
+
 end
